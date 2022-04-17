c2c1ff708a0a61ddf354b3457a6c33f1251e93b9
CryptoSnowPrince <topdirector2017@gmail.com>
2022-04-18 00:19:31 +0200

    update

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { DAppProvider } from '@usedapp/core';
+//redux store
+import { Provider } from 'react-redux'
+import store from './store';
 
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const rootElement = document.getElementById('root');
