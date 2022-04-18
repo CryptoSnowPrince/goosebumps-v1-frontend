35032689381faaefab29a847b59b0f83bf04f7bb
CryptoSnowPrince <topdirector2017@gmail.com>
2022-04-18 03:37:54 +0200

    remove un-used files

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,17 +1,7 @@
 import { combineReducers } from 'redux';
-import nftReducer from './nfts';
-import hotCollectionsReducer from './hotCollections';
-import authorListReducer from './authorList';
-import filterReducer from './filters';
-import blogPostsReducer from './blogs';
 import authReducer from './auth';
 
 export const rootReducer = combineReducers({
-  NFT: nftReducer,
-  hotCollection: hotCollectionsReducer,
-  authors: authorListReducer,
-  filters: filterReducer,
-  blogs: blogPostsReducer,
   auth: authReducer
 });
 
