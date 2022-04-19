a506c4f4c2e869f4a345df21e6f421c23b11c456
TonyTan <zymehan@gmail.com>
2022-04-19 21:47:23 +0300

    css fix

--- src/components/Submenu/Submenu.js
+++ src/components/Submenu/Submenu.js
@@ -6,7 +6,7 @@ import "./Submenu.scss"
 const Submenu = () => {
   const { pathname } = useLocation();
   return (
-    <div className='d-flex justify-content-center' id="swap_staking_farms">
+    <div className='d-flex justify-content-center mb-4' id="swap_staking_farms">
       <Link className={`nav-link mx-3 mx-xl-4 ${pathname === '/swap' ? "active" : ""}`}to="/swap">Swap</Link>
       <Link className={`nav-link mx-3 mx-xl-4 ${pathname === '/stake' ? "active" : ""}`} to="/stake">Staking</Link>
       <Link className={`nav-link mx-3 mx-xl-4 ${pathname === '/farms' ? "active" : ""}`} to="/farms">Farms</Link>
