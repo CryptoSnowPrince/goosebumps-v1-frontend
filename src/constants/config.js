6449a73911c58d30de14610a819fe26c063af315
CryptoSnowPrince <topdirector2017@gmail.com>
2022-04-20 22:32:25 +0200

    update network config

--- src/constants/config.js
+++ src/constants/config.js
@@ -3,11 +3,6 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const config = {
-    chainHexID: {
-        1: "0x1",
-        56: "0x38",
-        97: "0x61",
-    },
     INFURA_ID: process.env.REACT_APP_INFURA_ID,
 };
 
