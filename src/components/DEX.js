7e50c26cac3f70df6a3de0934d2a35778b2f0e9e
CryptoSnowPrince <topdirector2017@gmail.com>
2022-04-20 23:56:24 +0200

    switch net to ropsten

--- src/components/DEX.js
+++ src/components/DEX.js
@@ -4,12 +4,12 @@ import networks from "./../networks";
 import linq from "linq";
 
 const DEX = (props) => {
-    const network = linq.from(networks).where(x => x.Name === "bsctestnet").single();
+    const network = linq.from(networks).where(x => x.Name === "ropsten").single();
     // console.log(network);
 
     return (
         <div className="dex">
-            <Exchange network={network} fromSymbol="BNB" fromAddress="-" toSymbol="GBFT" toAddress="0xA6454bbA55F46Ef488c599c44867DF5eE3D6F543" />
+            <Exchange network={network} fromSymbol="ETH" fromAddress="-" toSymbol="GBFT" toAddress="0xA6454bbA55F46Ef488c599c44867DF5eE3D6F543" />
         </div>
     );
 }
