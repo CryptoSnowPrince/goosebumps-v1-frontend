33a9da83003a48494c3b7992f9eafdd3236a568d
CryptoSnowPrince <topdirector2017@gmail.com>
2022-04-22 20:14:39 +0200

    update DEX network

--- src/components/DEX.js
+++ src/components/DEX.js
@@ -2,9 +2,21 @@ import React, { useState, useEffect } from 'react';
 import { Exchange } from "./widgets/exchange/Exchange";
 import networks from "./../networks";
 import linq from "linq";
+import * as selector from '../../store/selectors';
 
 const DEX = (props) => {
-    const network = linq.from(networks).where(x => x.Name === "ropsten").single();
+    // const network = linq.from(networks).where(x => x.Name === "ropsten").single();
+    const selectedNetwork = useSelector(selector.chainState);
+    // const network = linq.from(networks).where(x => x.Name === "ropsten").single();
+    const [network, setNetwork] = useState(networks[localStorage.getItem("networkIndex") || 2]);
+
+    useEffect(() => {
+        try {
+            setNetwork(networks[selectedNetwork.chain.index]);
+        } catch (error) {
+            console.log("error: ", error);
+        }
+    }, [selectedNetwork])
     // console.log(network);
 
     return (
