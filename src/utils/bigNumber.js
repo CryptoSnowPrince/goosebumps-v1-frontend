6f2be50435f2aa6518781c1627922dfaec5b5a57
CryptoSnowPrince <topdirector2017@gmail.com>
2022-04-22 06:41:40 +0200

    add bignumber.js and hash.js packages update bigNumber.js

--- src/utils/bigNumber.js
+++ src/utils/bigNumber.js
@@ -6,7 +6,7 @@ export const BIG_ONE = new BigNumber(1)
 export const BIG_NINE = new BigNumber(9)
 export const BIG_TEN = new BigNumber(10)
 
-export const ethersToSerializedBigNumber = (ethersBn: ethers.BigNumber): SerializedBigNumber =>
+export const ethersToSerializedBigNumber = (ethersBn) =>
   ethersToBigNumber(ethersBn).toJSON()
 
-export const ethersToBigNumber = (ethersBn: ethers.BigNumber): BigNumber => new BigNumber(ethersBn.toString())
+export const ethersToBigNumber = (ethersBn) => new BigNumber(ethersBn.toString())
