03762dd56b50868447481493bb22b263a5a63652
CryptoSnowPrince <topdirector2017@gmail.com>
2022-04-23 08:41:55 +0200

    complete wallet connect and network switching

--- src/components/DEX.js
+++ src/components/DEX.js
@@ -8,7 +8,6 @@ import * as selector from '../store/selectors';
 const DEX = (props) => {
     // const network = linq.from(networks).where(x => x.Name === "ropsten").single();
     const selectedNetwork = useSelector(selector.chainState);
-    // const network = linq.from(networks).where(x => x.Name === "ropsten").single();
     const [network, setNetwork] = useState(networks[localStorage.getItem("networkIndex") || 2]);
 
     useEffect(() => {
@@ -18,12 +17,11 @@ const DEX = (props) => {
             console.log("error: ", error);
         }
     }, [selectedNetwork])
-    // console.log(network);
 
     return (
         <div className="dex">
-            <Exchange network={network}  />
             {/* <Exchange network={network} fromSymbol="WETH" fromAddress="0xc778417E063141139Fce010982780140Aa0cD5Ab" toSymbol="USDT" toAddress="0x110a13FC3efE6A245B50102D2d79B3E76125Ae83" /> */}
+            <Exchange network={network} fromSymbol={network.Currency.Name} fromAddress="-" toSymbol={network.Currency.WrappedName} toAddress={network.Currency.Address} />
         </div>
     );
 }
