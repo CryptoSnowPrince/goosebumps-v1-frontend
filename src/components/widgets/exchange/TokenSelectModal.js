7c9c99657f736bca508bce46e123bc1778d50f82
CryptoSnowPrince <topdirector2017@gmail.com>
2022-04-24 19:05:10 +0200

    complete token search with address, name and symbol

--- src/components/widgets/exchange/TokenSelectModal.js
+++ src/components/widgets/exchange/TokenSelectModal.js
@@ -13,11 +13,13 @@ const TokenSelectModal = (props) => {
 
     const [tokenSchKey, setTokenSchKey] = useState("");
 
-    const [schedTokens, setSchedTokens] = useState();
+    const [schedTokens, setSchedTokens] = useState([]);
 
     function onSelect(token) {
         props.onSelect(token, props.showFor);
         props.hide();
+        setSchedTokens();
+        setTokenSchKey("");
     }
 
     async function getTokenInfo(tokenAddress) {
@@ -81,15 +83,30 @@ const TokenSelectModal = (props) => {
     }, [init, props.networkName]);
 
     useEffect(() => {
-        // console.log("schedTokens: ", schedTokens);
-        // console.log("schedTokens length: ", schedTokens.length);
+        try {
+            // console.log("schedTokens: ", schedTokens);
+            // console.log("schedTokens length: ", schedTokens.length);
+            // console.log("typeof schedTokens: ", typeof schedTokens === Array);
+            // console.log("typeof schedTokens: ", typeof schedTokens);
+            // console.log("schedTokens: ", schedTokens);
+            // console.log("typeof tokens: ", typeof tokens);
+            // console.log("tokens: ", tokens);
+        } catch (error) {
+
+        }
     }, [schedTokens])
 
     useEffect(() => {
-        console.log("ethers.utils.isAddress(tokenSchKey): ", ethers.utils.isAddress(tokenSchKey))
+        // console.log("ethers.utils.isAddress(tokenSchKey): ", ethers.utils.isAddress(tokenSchKey))
         if (ethers.utils.isAddress(tokenSchKey)) {
-            console.log("okay");
-            getTokenInfo(tokenSchKey);
+            try {
+                const sched = linq.from(tokens.concat(tokensAddedByUser))
+                    .where(x => x.Address === tokenSchKey).toArray();
+                setSchedTokens(sched);
+            } catch (error) {
+                console.log("Token Search with address Err: ", error);
+            }
+            // getTokenInfo(tokenSchKey);
         }
         else if (tokenSchKey !== "") {
             try {
@@ -100,9 +117,8 @@ const TokenSelectModal = (props) => {
                         (x.Symbol.toLowerCase()).indexOf(tokenSchKey.toLowerCase()) !== -1)
                     ).toArray();
                 setSchedTokens(sched);
-                console.log(sched)
             } catch (error) {
-                console.log("Token Search Err: ", error);
+                console.log("Token Search with name or symbol Err: ", error);
             }
         }
     }, [tokens, tokensAddedByUser, tokenSchKey])
@@ -127,22 +143,22 @@ const TokenSelectModal = (props) => {
                     <div className='text-start overflow-auto border mt-3 p-3' style={{ maxHeight: 250 }}>
                         {
                             tokenSchKey !== "" ?
-                                ( schedTokens  ? "No results found." : "schedTokens"
-                                    // schedTokens.map((token, index) => (
-                                    //     <div key={index} className='row mb-3 align-items-center'>
-                                    //         <img className='col-auto' style={{ height: 32 }}
-                                    //             src={token.Logo ? token.Logo : "/assets/tokens/empty.png"}
-                                    //             alt={token.Symbol} />
-                                    //         <div className='col'>
-                                    //             <div>{token.Name}</div>
-                                    //             <div>{token.Symbol}</div>
-                                    //         </div>
-                                    //         <div className='col-auto'>
-                                    //             <button type="button" className="default-btn btn-sq"
-                                    //                 onClick={() => { onSelect(token) }}>Select</button>
-                                    //         </div>
-                                    //     </div>
-                                    // ))
+                                (schedTokens.length > 0 ?
+                                    schedTokens.map((token, index) => (
+                                        <div key={index} className='row mb-3 align-items-center'>
+                                            <img className='col-auto' style={{ height: 32 }}
+                                                src={token.Logo ? token.Logo : "/assets/tokens/empty.png"}
+                                                alt={token.Symbol} />
+                                            <div className='col'>
+                                                <div>{token.Name}</div>
+                                                <div>{token.Symbol}</div>
+                                            </div>
+                                            <div className='col-auto'>
+                                                <button type="button" className="default-btn btn-sq"
+                                                    onClick={() => { onSelect(token) }}>Select</button>
+                                            </div>
+                                        </div>
+                                    )) : "No results found."
                                 ) :
                                 (tokens.concat(tokensAddedByUser)).map((token, index) => (
                                     <div key={index} className='row mb-3 align-items-center'>
@@ -165,6 +181,7 @@ const TokenSelectModal = (props) => {
                 <Modal.Footer className="border-info">
                     <button type="button" className="default-btn btn-sq"
                         onClick={() => {
+                            setSchedTokens();
                             setTokenSchKey("");
                             props.hide();
                         }}>Close</button>
