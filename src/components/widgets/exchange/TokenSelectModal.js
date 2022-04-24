1d5e74b8a4d7d788cc0304c9b2038fd5d6278787
CryptoSnowPrince <topdirector2017@gmail.com>
2022-04-24 17:58:03 +0200

    search token list with token name and symbol reset searchKeyword when close TokenSelectModal

--- src/components/widgets/exchange/TokenSelectModal.js
+++ src/components/widgets/exchange/TokenSelectModal.js
@@ -7,10 +7,13 @@ import linq from "linq";
 
 const TokenSelectModal = (props) => {
     const [init, setInit] = useState(true);
+
     const [tokens, setTokens] = useState();
     const [tokensAddedByUser, setTokensAddedByUser] = useState();
 
-    const [searchTokens, setSearchTokens] = useState("");
+    const [tokenSchKey, setTokenSchKey] = useState("");
+
+    const [schedTokens, setSchedTokens] = useState();
 
     function onSelect(token) {
         props.onSelect(token, props.showFor);
@@ -78,20 +81,31 @@ const TokenSelectModal = (props) => {
     }, [init, props.networkName]);
 
     useEffect(() => {
-
-    }, [])
+        // console.log("schedTokens: ", schedTokens);
+        // console.log("schedTokens length: ", schedTokens.length);
+    }, [schedTokens])
 
     useEffect(() => {
-        console.log("typeof tokens: ", typeof tokens)
-        console.log("tokens: ", tokens)
-        console.log("searchTokens: ", searchTokens)
-        console.log("ethers.utils.isAddress(searchTokens): ", ethers.utils.isAddress(searchTokens))
-        if (ethers.utils.isAddress(searchTokens)) {
+        console.log("ethers.utils.isAddress(tokenSchKey): ", ethers.utils.isAddress(tokenSchKey))
+        if (ethers.utils.isAddress(tokenSchKey)) {
             console.log("okay");
-            getTokenInfo(searchTokens);
+            getTokenInfo(tokenSchKey);
         }
-
-    }, [tokens, searchTokens])
+        else if (tokenSchKey !== "") {
+            try {
+                const sched = linq.from(tokens.concat(tokensAddedByUser))
+                    .where(x =>
+                    (
+                        (x.Name.toLowerCase()).indexOf(tokenSchKey.toLowerCase()) !== -1 ||
+                        (x.Symbol.toLowerCase()).indexOf(tokenSchKey.toLowerCase()) !== -1)
+                    ).toArray();
+                setSchedTokens(sched);
+                console.log(sched)
+            } catch (error) {
+                console.log("Token Search Err: ", error);
+            }
+        }
+    }, [tokens, tokensAddedByUser, tokenSchKey])
 
     if (!props.showFor || init) {
         return null;
@@ -108,47 +122,52 @@ const TokenSelectModal = (props) => {
                 <Modal.Body className="text-center">
                     <input type="text" className='form-control'
                         placeholder="Search name or paste address"
-                        onChange={(e) => { setSearchTokens(e.target.value) }} />
+                        onChange={(e) => { setTokenSchKey(e.target.value) }} />
 
                     <div className='text-start overflow-auto border mt-3 p-3' style={{ maxHeight: 250 }}>
                         {
-                            tokens.map((token, index) => (
-                                <div key={index} className='row mb-3 align-items-center'>
-                                    <img className='col-auto' style={{ height: 32 }}
-                                        src={token.Logo ? token.Logo : "/assets/tokens/empty.png"}
-                                        alt={token.Symbol} />
-                                    <div className='col'>
-                                        <div>{token.Name}</div>
-                                        <div>{token.Symbol}</div>
+                            tokenSchKey !== "" ?
+                                ( schedTokens  ? "No results found." : "schedTokens"
+                                    // schedTokens.map((token, index) => (
+                                    //     <div key={index} className='row mb-3 align-items-center'>
+                                    //         <img className='col-auto' style={{ height: 32 }}
+                                    //             src={token.Logo ? token.Logo : "/assets/tokens/empty.png"}
+                                    //             alt={token.Symbol} />
+                                    //         <div className='col'>
+                                    //             <div>{token.Name}</div>
+                                    //             <div>{token.Symbol}</div>
+                                    //         </div>
+                                    //         <div className='col-auto'>
+                                    //             <button type="button" className="default-btn btn-sq"
+                                    //                 onClick={() => { onSelect(token) }}>Select</button>
+                                    //         </div>
+                                    //     </div>
+                                    // ))
+                                ) :
+                                (tokens.concat(tokensAddedByUser)).map((token, index) => (
+                                    <div key={index} className='row mb-3 align-items-center'>
+                                        <img className='col-auto' style={{ height: 32 }}
+                                            src={token.Logo ? token.Logo : "/assets/tokens/empty.png"}
+                                            alt={token.Symbol} />
+                                        <div className='col'>
+                                            <div>{token.Name}</div>
+                                            <div>{token.Symbol}</div>
+                                        </div>
+                                        <div className='col-auto'>
+                                            <button type="button" className="default-btn btn-sq"
+                                                onClick={() => { onSelect(token) }}>Select</button>
+                                        </div>
                                     </div>
-                                    <div className='col-auto'>
-                                        <button type="button" className="default-btn btn-sq"
-                                            onClick={() => { onSelect(token) }}>Select</button>
-                                    </div>
-                                </div>
-                            ))
-                        }
-                        {
-                            tokensAddedByUser.map((token, index) => (
-                                <div key={index} className='row mb-3 align-items-center'>
-                                    <img className='col-auto' style={{ height: 32 }}
-                                        src={"/assets/tokens/empty.png"}
-                                        alt={token.Symbol} />
-                                    <div className='col'>
-                                        <div>{token.Name}</div>
-                                        <div>{token.Symbol}</div>
-                                    </div>
-                                    <div className='col-auto'>
-                                        <button type="button" className="default-btn btn-sq"
-                                            onClick={() => { onSelect(token) }}>Select</button>
-                                    </div>
-                                </div>
-                            ))
+                                ))
                         }
                     </div>
                 </Modal.Body>
                 <Modal.Footer className="border-info">
-                    <button type="button" className="default-btn btn-sq" onClick={props.hide}>Close</button>
+                    <button type="button" className="default-btn btn-sq"
+                        onClick={() => {
+                            setTokenSchKey("");
+                            props.hide();
+                        }}>Close</button>
                 </Modal.Footer>
             </div>
         </Modal>
