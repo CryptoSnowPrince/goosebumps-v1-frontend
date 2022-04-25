713806239bfa01b676c371b23bfd137890095a59
CryptoSnowPrince <topdirector2017@gmail.com>
2022-04-25 13:39:41 +0200

    remove swap on submenu

--- src/components/Submenu/Submenu.js
+++ src/components/Submenu/Submenu.js
@@ -7,7 +7,6 @@ const Submenu = () => {
   const { pathname } = useLocation();
   return (
     <div className='d-flex justify-content-center mb-4' id="swap_staking_farms">
-      <Link className={`nav-link mx-3 mx-xl-4 ${pathname === '/swap' ? "active" : ""}`}to="/swap">Swap</Link>
       <Link className={`nav-link mx-3 mx-xl-4 ${pathname === '/stake' ? "active" : ""}`} to="/stake">Staking</Link>
       <Link className={`nav-link mx-3 mx-xl-4 ${pathname === '/farms' ? "active" : ""}`} to="/farms">Farms</Link>
     </div>
