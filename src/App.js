97db7aadc09afb911ebefde09a93aa5d5376d6f9
CryptoSnowPrince <topdirector2017@gmail.com>
2022-04-27 17:03:19 +0200

    add auto connect to wallet when load App

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import { Route, Routes } from 'react-router';
 import { Layout } from './components/Layout';
 import { Home } from './components/Home';
@@ -13,36 +13,38 @@ import { LiquidityRemove } from './pages/LiquidityRemove/LiquidityRemove';
 import { LiquidityFindToken } from './pages/LiquidityFindToken/LiquidityFindToken';
 import { Farming } from './pages/Farming/Farming';
 import { Bridge } from './pages/Bridge/Bridge';
+import { Connect } from './components/widgets/connectWallet';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+const App = () => {
+  useEffect(() => {
+    Connect();
+  }, []);
 
-export default class App extends Component {
-    static displayName = App.name;
-
-    render() {
-        return (
-            <Layout>
-                <Routes>
-                    <Route exact path='/' element={<Home />} />
-                    <Route exact path='/stake' element={<Staking />} />
-                    <Route exact path='/liquidity' element={<Liquidity />} />
-                    <Route exact path='/liquidityAdd' element={<LiquidityAdd />} />
-                    <Route exact path='/liquidityRemove' element={<LiquidityRemove />} />
-                    <Route exact path='/liquidityFindToken' element={<LiquidityFindToken />} />
-                    <Route exact path='/farms' element={<Farming />} />
-                    <Route exact path='/bridge' element={<Bridge />} />
-                    <Route path='/portfolio-tracker' element={<PortfolioTracker />}>
-                        <Route path='/portfolio-tracker/:networkName/:addresses' element={<PortfolioTracker />} />
-                    </Route>
-                    <Route path='/charts' element={<Charts />}>
-                        <Route path='/charts/:networkName/:address' element={<Charts />}>
-                            <Route path='/charts/:networkName/:address/:pairAddress' element={<Charts />} />
-                        </Route>
-                    </Route>
-                    <Route path='/dex' element={<DEX />} />
-                </Routes>
-            </Layout>
-        );
-    }
+  return (
+    <Layout>
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route exact path='/stake' element={<Staking />} />
+        <Route exact path='/liquidity' element={<Liquidity />} />
+        <Route exact path='/liquidityAdd' element={<LiquidityAdd />} />
+        <Route exact path='/liquidityRemove' element={<LiquidityRemove />} />
+        <Route exact path='/liquidityFindToken' element={<LiquidityFindToken />} />
+        <Route exact path='/farms' element={<Farming />} />
+        <Route exact path='/bridge' element={<Bridge />} />
+        <Route path='/portfolio-tracker' element={<PortfolioTracker />}>
+          <Route path='/portfolio-tracker/:networkName/:addresses' element={<PortfolioTracker />} />
+        </Route>
+        <Route path='/charts' element={<Charts />}>
+          <Route path='/charts/:networkName/:address' element={<Charts />}>
+            <Route path='/charts/:networkName/:address/:pairAddress' element={<Charts />} />
+          </Route>
+        </Route>
+        <Route path='/dex' element={<DEX />} />
+      </Routes>
+    </Layout>
+  )
 }
+
+export default { App };
